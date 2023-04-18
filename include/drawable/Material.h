6b4f697f5e13a014fae81478dbc5e80a933435d1
hescul <ndminh1101@gmail.com>
2023-04-18 13:03:40 +0700

    Add a bunch of Phong materials
    Signed-off-by: hescul <ndminh1101@gmail.com>

--- include/drawable/Material.h
+++ include/drawable/Material.h
@@ -7,10 +7,10 @@
 
 namespace phong {
     struct Material {
-        const glm::vec3 ambient;
-        const glm::vec3 diffuse;
-        const glm::vec3 specular;
-        const float shininess;
+        glm::vec3 ambient;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
+        float shininess;
     };
 
     static constexpr auto EMERALD = Material{
@@ -39,5 +39,61 @@ namespace phong {
             glm::vec3{ 1.0f, 0.829f, 0.829f },
             glm::vec3{ 0.296648f, 0.296648f, 0.296648f },
             0.088f * 128
+    };
+
+    static constexpr auto RUBY = Material{
+            glm::vec3{ 0.1745f, 0.01175f, 0.01175f },
+            glm::vec3{ 0.61424f, 0.04136f, 0.04136f },
+            glm::vec3{ 0.727811f, 0.626959f, 0.626959f },
+            0.6f * 128
+    };
+
+    static constexpr auto TURQUOISE = Material{
+            glm::vec3{ 0.1f, 0.18725f, 0.1745f },
+            glm::vec3{ 0.396f, 0.74151f, 0.69102f },
+            glm::vec3{ 0.297254f, 0.30829f, 0.306678f },
+            0.1f * 128
+    };
+
+    static constexpr auto COPPER = Material{
+            glm::vec3{ 0.19125f, 0.0735f, 0.0225f },
+            glm::vec3{ 0.7038f, 0.27048f, 0.0828f },
+            glm::vec3{ 0.256777f, 0.137622f, 0.086014f },
+            0.1f * 128
+    };
+
+    static constexpr auto SILVER = Material{
+            glm::vec3{ 0.19225f, 0.19225f, 0.19225f },
+            glm::vec3{ 0.50754f, 0.50754f, 0.50754f },
+            glm::vec3{ 0.508273f,	0.508273f, 0.508273f },
+            0.4f * 128
+    };
+
+    static constexpr auto GOLD = Material{
+            glm::vec3{ 0.24725f, 0.1995f, 0.0745f },
+            glm::vec3{ 0.75164f, 0.60648f, 0.22648f },
+            glm::vec3{ 0.628281f, 0.555802f,	0.366065f },
+            0.4f * 128
+    };
+
+    static constexpr auto CHROME = Material{
+            glm::vec3{ 0.25f, 0.25f, 0.25f },
+            glm::vec3{ 0.4f, 0.4f, 0.4f },
+            glm::vec3{ 0.774597f, 0.774597f, 0.774597f },
+            0.6f * 128
+    };
+
+    static constexpr auto WHITE_PLASTIC = Material{
+            glm::vec3{ 0.0f, 0.0f, 0.0f },
+            glm::vec3{ 0.55f, 0.55f, 0.55f },
+            glm::vec3{ 0.70f, 0.70f, 0.70f },
+            0.25f * 128
+    };
+
+    static constexpr auto CYAN_PLASTIC = Material{
+            glm::vec3{ 0.0f, 0.1f, 0.06f },
+            glm::vec3{ 0.0f, 0.50980392f, 0.50980392f },
+            glm::vec3{ 0.50196078f, 0.50196078f, 0.50196078f },
+            0.25f * 128
     };
 }
