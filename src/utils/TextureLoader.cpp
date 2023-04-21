58d294a1eba68cbb22199e2a7e24fe5e8e46106e
hescul <ndminh1101@gmail.com>
2023-04-21 13:58:40 +0700

    Turn off vertical flipping on texture load
    Signed-off-by: hescul <ndminh1101@gmail.com>

--- src/utils/TextureLoader.cpp
+++ src/utils/TextureLoader.cpp
@@ -13,9 +13,7 @@ Texture::Format format(int channels);
 Texture* loadTexture(const std::string_view name, Engine& engine) {
     const auto path = std::string{ "res/textures/" } + name.data();
     int width, height, channels;
-    // OpenGL expects the 0.0 coordinate on the y-axis to be on the bottom side of the image,
-    // but images usually have 0.0 at the top of the y-axis.
-    stbi_set_flip_vertically_on_load(true);
+    stbi_set_flip_vertically_on_load(false);
     const auto data = stbi_load(path.data(), &width, &height, &channels, 0);
     if (data) {
         std::cout << name << ": ";
