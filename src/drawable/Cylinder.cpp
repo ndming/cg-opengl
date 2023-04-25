b54e290b6f5040c015f8150dafce5b0f9a47c91a
hescul <ndminh1101@gmail.com>
2023-04-25 18:24:27 +0700

    Add Phong support for `Cylinder` drawable
    Signed-off-by: hescul <ndminh1101@gmail.com>

--- src/drawable/Cylinder.cpp
+++ src/drawable/Cylinder.cpp
@@ -21,69 +21,75 @@ Cylinder::Builder& Cylinder::Builder::segments(const int segments) {
 std::unique_ptr<Drawable> Cylinder::Builder::build(Engine& engine) {
 	auto positions = std::vector<float>{};
 	auto colors = std::vector<float>{};
+    auto normals = std::vector<float>{};
 
-	const auto baseCenter = glm::vec3{ 0.0f, 0.0f, -1.0f };
 	const auto up = glm::vec3{ 0.0f, 0.0f, 1.0f };
 
-	// Do this twice for two base circles
-	for (auto i = 0; i < 2; ++i) {
-		// the actual center
-		const auto center = baseCenter + up * (2.0f * static_cast<float>(i));
+    // Top center vertex
+    const auto topCenter = glm::vec3{ 0.0f, 0.0f, 1.0f };
+    positions.push_back(topCenter.x); positions.push_back(topCenter.y); positions.push_back(topCenter.z);
+    colors.push_back(srgb::YELLOW[0]); colors.push_back(srgb::YELLOW[1]); colors.push_back(srgb::YELLOW[2]); colors.push_back(1.0f);
+    normals.push_back(topCenter.x); normals.push_back(topCenter.y); normals.push_back(topCenter.z);
 
-		// center vertex
-		positions.push_back(center.x);
-		positions.push_back(center.y);
-		positions.push_back(center.z);
-		colors.push_back(srgb::YELLOW[0]);
-		colors.push_back(srgb::YELLOW[1]);
-		colors.push_back(srgb::YELLOW[2]);
-		colors.push_back(1.0f);
+	// Construct circular vertices, doing this twice for each base circles for accurate normal mapping. This means
+    // we are looping around 4 times
+	for (auto i = 0; i < 4; ++i) {
+		// The actual center, for transform reference only
+        const auto multiplier = i / 2;
+		const auto center = topCenter - up * (2.0f * static_cast<float>(multiplier));
 
-		// circular vertices
-		for (auto j = 0; j < _segments; ++j) {
-			// the angle of rotation
+		// Circular vertices, repeated at the start and end for accurate texture mapping
+		for (auto j = 0; j <= _segments; ++j) {
+			// The angle of rotation
 			const auto angle = static_cast<float>(j) * 2.0f * 
 				std::numbers::pi_v<float> / static_cast<float>(_segments);
-			// rotation vector on the XY-plane
+			// Rotation vector on the XY-plane
 			const auto rot = glm::vec3{ std::cos(angle), std::sin(angle), 0.0f };
-			// the direction to the point on circle
+			// The direction to the point on circle
 			const auto dir = normalize(cross(up, rot));
-			// translate to that point
+			// Translate to that point
 			const auto point = center + dir * 1.0f;
 			positions.push_back(point.x); positions.push_back(point.y); positions.push_back(point.z);
 			colors.push_back(srgb::BROWN[0]); colors.push_back(srgb::BROWN[1]);
 			colors.push_back(srgb::BROWN[2]); colors.push_back(1.0f);
+            normals.push_back(point.x); normals.push_back(point.y); normals.push_back(point.z);
 		}
 	}
 
+    // Bottom center vertex
+    const auto botCenter = glm::vec3{ 0.0f, 0.0f, -1.0f };
+    positions.push_back(botCenter.x); positions.push_back(botCenter.y); positions.push_back(botCenter.z);
+    colors.push_back(srgb::YELLOW[0]); colors.push_back(srgb::YELLOW[1]); colors.push_back(srgb::YELLOW[2]); colors.push_back(1.0f);
+    normals.push_back(botCenter.x); normals.push_back(botCenter.y); normals.push_back(botCenter.z);
+
+    const auto vertexCount = static_cast<int>(positions.size() / 3);
+
 	auto topIndices = std::vector{ 0u };
-	for (auto i = 0; i < _segments; ++i) {
+	for (auto i = 0; i <= _segments; ++i) {
 		topIndices.push_back(i + 1);
 	}
-	topIndices.push_back(1u);
 
-	auto botIndices = std::vector{ static_cast<unsigned>(_segments + 1) };
-	for (auto i = 0; i < _segments; ++i) {
-		botIndices.push_back(i + _segments + 2);
+	auto botIndices = std::vector{ static_cast<unsigned>(vertexCount - 1) };
+	for (auto i = 0; i <= _segments; ++i) {
+		botIndices.push_back(static_cast<unsigned>(vertexCount - i - 2));
 	}
-	botIndices.push_back(_segments + 2);
 
 	auto sideIndices = std::vector<unsigned>{};
-	for (auto i = 0; i < _segments; ++i) {
-		sideIndices.push_back(i + 2 + _segments);
-		sideIndices.push_back(i + 1);
+	for (auto i = 0; i <= _segments; ++i) {
+        sideIndices.push_back(i + 1 + 1 * (_segments + 1));
+		sideIndices.push_back(i + 1 + 2 * (_segments + 1));
 	}
-	sideIndices.push_back(2 + _segments);
-	sideIndices.push_back(1u);
 
 	constexpr auto floatSize = 4;
-	const auto vertexBuffer = VertexBuffer::Builder(2)
-		.vertexCount(static_cast<int>(positions.size() / 3))
+	const auto vertexBuffer = VertexBuffer::Builder(3)
+		.vertexCount(vertexCount)
 		.attribute(0, VertexBuffer::VertexAttribute::POSITION, VertexBuffer::AttributeType::FLOAT3, 0, floatSize * 3)
 		.attribute(1, VertexBuffer::VertexAttribute::COLOR, VertexBuffer::AttributeType::FLOAT4, 0, floatSize * 4)
-		.build(engine);
+		.attribute(2, VertexBuffer::VertexAttribute::NORMAL, VertexBuffer::AttributeType::FLOAT3, 0, floatSize * 3)
+        .build(engine);
 	vertexBuffer->setBufferAt(0, positions.data());
 	vertexBuffer->setBufferAt(1, colors.data());
+    vertexBuffer->setBufferAt(2, normals.data());
 
 	const auto topBuffer = IndexBuffer::Builder()
 		.indexCount(static_cast<int>(topIndices.size()))
@@ -104,23 +110,21 @@ std::unique_ptr<Drawable> Cylinder::Builder::build(Engine& engine) {
 	sideBuffer->setBuffer(sideIndices.data());
 
 	const auto shader = Shader::Builder(_shaderModel).build(engine);
-	const auto entity = EntityManager::get()->create();
+    if (_shaderModel == Shader::Model::PHONG) {
+        shader->use();
+        shader->setUniform(Shader::Uniform::MATERIAL_AMBIENT, _phongAmbient.r, _phongAmbient.g, _phongAmbient.b);
+        shader->setUniform(Shader::Uniform::MATERIAL_DIFFUSE, _phongDiffuse.r, _phongDiffuse.g, _phongDiffuse.b);
+        shader->setUniform(Shader::Uniform::MATERIAL_SPECULAR, _phongSpecular.r, _phongSpecular.g, _phongSpecular.b);
+        shader->setUniform(Shader::Uniform::MATERIAL_SHININESS, _phongShininess);
+    }
 
+    const auto entity = EntityManager::get()->create();
 	RenderableManager::Builder(3)
-		.geometry(
-			0, RenderableManager::PrimitiveType::TRIANGLE_STRIP, *vertexBuffer, *sideBuffer,
-			static_cast<int>(sideIndices.size()), 0
-		)
+		.geometry(0, RenderableManager::PrimitiveType::TRIANGLE_STRIP, *vertexBuffer, *sideBuffer,static_cast<int>(sideIndices.size()), 0)
 		.shader(0, shader)
-		.geometry(
-			1, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *topBuffer,
-			static_cast<int>(topIndices.size()), 0
-		)
+		.geometry(1, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *topBuffer,static_cast<int>(topIndices.size()), 0)
 		.shader(1, shader)
-		.geometry(
-			2, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *botBuffer,
-			static_cast<int>(botIndices.size()), 0
-		)
+		.geometry(2, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *botBuffer,static_cast<int>(botIndices.size()), 0)
 		.shader(2, shader)
 		.build(entity);
 
