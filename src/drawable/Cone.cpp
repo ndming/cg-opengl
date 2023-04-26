23c226210fa14c3b234444f2c7e1084771aacd79
hescul <ndminh1101@gmail.com>
2023-04-26 13:28:03 +0700

    Add Phong support for `Cone` drawable
    Signed-off-by: hescul <ndminh1101@gmail.com>

--- src/drawable/Cone.cpp
+++ src/drawable/Cone.cpp
@@ -13,85 +13,100 @@
 #include "drawable/Cone.h"
 #include "drawable/Color.h"
 
+using namespace srgb;
+
 Cone::Builder& Cone::Builder::segments(const int segments) {
 	_segments = segments;
 	return *this;
 }
 
 std::unique_ptr<Drawable> Cone::Builder::build(Engine& engine) {
-	const auto baseCenter = glm::vec3{ 0.0f, 0.0f, -1.0f };
-	const auto up = glm::vec3{ 0.0f, 0.0f, 1.0f };
+    auto positions = std::vector<float>{};
+    auto colors = std::vector<float>{};
+    auto normals = std::vector<float>{};
 
-	auto positions = std::vector{
-		baseCenter.x, baseCenter.y, baseCenter.z
-	};
-	auto colors = std::vector{
-		srgb::CYAN[0], srgb::CYAN[1], srgb::CYAN[2], 1.0f
-	};
+    const auto baseCenter = glm::vec3{ 0.0f, 0.0f, -1.0f };
+    const auto up = glm::vec3{ 0.0f, 0.0f, 1.0f };
 
-	// Base circle
-	for (auto i = 0; i < _segments; ++i) {
-		const auto angle = static_cast<float>(i) * 2.0f * 
-			std::numbers::pi_v<float> / static_cast<float>(_segments);
-		const auto rot = glm::vec3{ std::cos(angle), std::sin(angle), 0.0f };
-		const auto dir = normalize(cross(up, rot));
-		const auto point = baseCenter + dir * 1.0f;
-		positions.push_back(point.x); positions.push_back(point.y); positions.push_back(point.z);
-		colors.push_back(srgb::PURPLE[0]); colors.push_back(srgb::PURPLE[1]);
-		colors.push_back(srgb::PURPLE[2]); colors.push_back(1.0f);
+    // Top vertex
+    const auto top = baseCenter + up * 2.0f;
+    positions.push_back(top.x); positions.push_back(top.y); positions.push_back(top.z);
+    colors.push_back(RED[0]); colors.push_back(RED[1]); colors.push_back(RED[2]); colors.push_back(1.0f);
+    normals.push_back(up.x); normals.push_back(up.y); normals.push_back(up.z);
+
+	// Circular vertices, twice for accurate normal mapping
+    for (auto i = 0; i < 2; ++i) {
+        // Repeat the first and last vertex for accurate texture mapping
+        for (auto j = 0; j <= _segments; ++j) {
+            const auto angle = static_cast<float>(j) * 2.0f * std::numbers::pi_v<float> / static_cast<float>(_segments);
+            const auto dir = glm::normalize(glm::vec3{ std::cos(angle), std::sin(angle), 0.0f });
+            const auto point = baseCenter + dir * 1.0f;
+            positions.push_back(point.x); positions.push_back(point.y); positions.push_back(point.z);
+            colors.push_back(PURPLE[0]); colors.push_back(PURPLE[1]); colors.push_back(PURPLE[2]); colors.push_back(1.0f);
+            if (i == 0) {
+                const auto tangent = glm::cross(up, dir);
+                const auto norm = glm::normalize(glm::cross(tangent, top - point));
+                normals.push_back(norm.x); normals.push_back(norm.y); normals.push_back(norm.z);
+            } else {
+                normals.push_back(-up.x); normals.push_back(-up.y); normals.push_back(-up.z);
+            }
+        }
+    }
+
+    // Additional bottom center vertex for circle drawing with GL_TRIANGLE_FAN
+    positions.push_back(baseCenter.x); positions.push_back(baseCenter.y); positions.push_back(baseCenter.z);
+    colors.push_back(CYAN[0]); colors.push_back(CYAN[1]); colors.push_back(CYAN[2]); colors.push_back(1.0f);
+    normals.push_back(-up.x); normals.push_back(-up.y); normals.push_back(-up.z);
+
+    // We will need 2 index buffers to draw the cone's side and the cone's base circle
+    const auto vertexCount = static_cast<int>(positions.size() / 3);
+	auto baseIndices = std::vector{ static_cast<unsigned>(vertexCount - 1) };
+	for (auto i = 0; i <= _segments; ++i) {
+		baseIndices.push_back(vertexCount - 2 - i);
 	}
-	// Top vertex
-	const auto top = baseCenter + up * 2.0f;
-	positions.push_back(top.x); positions.push_back(top.y); positions.push_back(top.z);
-	colors.push_back(srgb::RED[0]); colors.push_back(srgb::RED[1]);
-	colors.push_back(srgb::RED[2]); colors.push_back(1.0f);
 
-	auto circleIndices = std::vector{ 0u };
-	for (auto i = 0; i < _segments; ++i) {
-		circleIndices.push_back(i + 1);
+	auto sideIndices = std::vector{ 0u };
+	for (auto i = 0; i <= _segments; ++i) {
+		sideIndices.push_back(i + 1);
 	}
-	circleIndices.push_back(1u);
-
-	auto coneIndices = std::vector{ static_cast<unsigned>(_segments + 1) };
-	for (auto i = 0; i < _segments; ++i) {
-		coneIndices.push_back(i + 1);
-	}
-	coneIndices.push_back(1u);
 
 	constexpr auto floatSize = 4;
-	const auto vertexBuffer = VertexBuffer::Builder(2)
-		.vertexCount(static_cast<int>(positions.size() / 3))
+	const auto vertexBuffer = VertexBuffer::Builder(3)
+		.vertexCount(vertexCount)
 		.attribute(0, VertexBuffer::VertexAttribute::POSITION, VertexBuffer::AttributeType::FLOAT3, 0, floatSize * 3)
 		.attribute(1, VertexBuffer::VertexAttribute::COLOR, VertexBuffer::AttributeType::FLOAT4, 0, floatSize * 4)
-		.build(engine);
+		.attribute(2, VertexBuffer::VertexAttribute::NORMAL, VertexBuffer::AttributeType::FLOAT3, 0, floatSize * 3)
+        .build(engine);
 	vertexBuffer->setBufferAt(0, positions.data());
 	vertexBuffer->setBufferAt(1, colors.data());
+    vertexBuffer->setBufferAt(2, normals.data());
 
-	const auto circleBuffer = IndexBuffer::Builder()
-		.indexCount(static_cast<int>(circleIndices.size()))
+	const auto baseIdxBuff = IndexBuffer::Builder()
+		.indexCount(static_cast<int>(baseIndices.size()))
 		.indexType(IndexBuffer::Builder::IndexType::UINT)
 		.build(engine);
-	circleBuffer->setBuffer(circleIndices.data());
+	baseIdxBuff->setBuffer(baseIndices.data());
 
-	const auto coneBuffer = IndexBuffer::Builder()
-		.indexCount(static_cast<int>(coneIndices.size()))
+	const auto sideIdxBuff = IndexBuffer::Builder()
+		.indexCount(static_cast<int>(sideIndices.size()))
 		.indexType(IndexBuffer::Builder::IndexType::UINT)
 		.build(engine);
-	coneBuffer->setBuffer(coneIndices.data());
+	sideIdxBuff->setBuffer(sideIndices.data());
 
 	const auto shader = Shader::Builder(_shaderModel).build(engine);
-	const auto entity = EntityManager::get()->create();
+    if (_shaderModel == Shader::Model::PHONG) {
+        shader->use();
+        shader->setUniform(Shader::Uniform::MATERIAL_AMBIENT, _phongAmbient.r, _phongAmbient.g, _phongAmbient.b);
+        shader->setUniform(Shader::Uniform::MATERIAL_DIFFUSE, _phongDiffuse.r, _phongDiffuse.g, _phongDiffuse.b);
+        shader->setUniform(Shader::Uniform::MATERIAL_SPECULAR, _phongSpecular.r, _phongSpecular.g, _phongSpecular.b);
+        shader->setUniform(Shader::Uniform::MATERIAL_SHININESS, _phongShininess);
+    }
 
+	const auto entity = EntityManager::get()->create();
 	RenderableManager::Builder(2)
-		.geometry(
-			0, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *circleBuffer,
-			static_cast<int>(circleIndices.size()), 0
-		)
+		.geometry(0, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *baseIdxBuff, static_cast<int>(baseIndices.size()), 0)
 		.shader(0, shader)
-		.geometry(
-			1, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *coneBuffer,
-			static_cast<int>(coneIndices.size()), 0
-		)
+		.geometry(1, RenderableManager::PrimitiveType::TRIANGLE_FAN, *vertexBuffer, *sideIdxBuff, static_cast<int>(sideIndices.size()), 0)
 		.shader(1, shader)
 		.build(entity);
 
